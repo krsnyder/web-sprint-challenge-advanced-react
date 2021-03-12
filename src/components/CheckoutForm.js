f628c191c7a74f09ab38bd06bd2b59e38ce9d17d
Kirk <kirkrobertsnyder@gmail.com>
2021-03-12 11:36:36 -0500

    Wrote and passed all required tests

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -14,9 +14,6 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-
-
-
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, handleChanges] = useForm(initialValue)
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+  const head = screen.queryByText(/checkout form/i)
+
+  expect(head).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", async() => {
+  //Arrange
+  render(<CheckoutForm />)
+
+  const firstNameInput = screen.getByLabelText(/first name/i)
+  const lastNameInput = screen.getByLabelText(/last name/i)
+  const addressInput = screen.getByLabelText(/address/i)
+  const cityInput = screen.getByLabelText(/city/i)
+  const stateInput = screen.getByLabelText(/state/i)
+  const zipInput = screen.getByLabelText(/zip/i)
+
+  const button = screen.getByRole("button")
+
+  //Act
+  userEvent.type(firstNameInput, "Nardwuar")
+  userEvent.type(lastNameInput, "the Human Serviette")
+  userEvent.type(addressInput, "309 Kingizz Ln")
+  userEvent.type(cityInput, "Asbury Park")
+  userEvent.type(stateInput, "NJ")
+  userEvent.type(zipInput, "00000")
+
+  userEvent.click(button)
+
+
+  //Assert
+  await waitFor(() => {
+    const successMessage = screen.getByTestId("successMessage")
+    expect(successMessage).toBeInTheDocument()
+  })
+});
